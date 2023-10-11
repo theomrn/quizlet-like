636b9fc32fc700e3c6f2ffc7fceda4b8d5112273
theo <theomorneau@icloud.com>
2023-10-11 11:33:09 +0200

    v3

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from random import *
 
-Fr1 = ["(taux de) présence du personnel","dédaigneux / condéscendant","éloge","évaluateur, expert","motivation","acceptant / contestant"
-       ,"alors que , tandis que","par la présence","intérimaire","prestataire","poste vacant"]
+Fr1 = ["(taux de) présence du personnel" , "dédaigneux / condéscendant","éloge","évaluateur, expert","motivation","acceptant / contestant"
+       ,"alors que , tandis que","par la présente","intérimaire","prestataire","poste vacant"]
 
-En1 = ["Staff attendance","dismissive","praise","assersor","incentive","agreeing"
-       ,"whereas","herby","temp","contractor","vacancy"]
+En1 = ["Staff attendance","dismissive","praise","assesor","incentive","agreeing"
+       ,"whereas","hereby","temp","contractor","vacancy"]
 
 Fr2 = ["main d'oeuvre, effectifs","embaucher","réduire les effectifs","licencier pour motif économique","donner son preavis","renvoyer","licencié"
        ,"licenciment","nommer","évaluation","évaluer, estimer","demandeur, requérant"]
@@ -15,7 +15,7 @@ En2 = ["manpower","take on","downsize","lay off","give notice","dismiss","dismis
 Fr3 = ["demande, requête","évaluer","évaluation,analyse","démissionner","signe, panneau","démissions"
        ,"mutation, muter","transférant / cédant","sélectionner","employer"]
 
-En3 = ["application","asses","assessment","resign","sign","resignation"
+En3 = ["application","asses","assessement","resign","sign","resignation"
        ,"trasfer","transferring","shortlisting","employ"]
 
 Fr4 = ["emploi","bulletin de paie","heures supplémentaires","avantages en nature","bénévole","avenant"
@@ -62,12 +62,7 @@ En10 = ["binder","wage","affraid of","capable of","jalous of","accuse somebody o
 En = [En1,En2,En3,En4,En5,En6,En7,En8,En9,En10]
 Fr = [Fr1,Fr2,Fr3,Fr4,Fr5,Fr6,Fr7,Fr8,Fr9,Fr10]
 
-
-
-
-
-
-
+print(len(En[0]))
 
 continuer = "oui"
 
@@ -75,8 +70,8 @@ while (continuer == "oui"):
        for k in range(0,9):
               i = randint(0, 9)
               j = randint(0, len(En[i]))
-              print(Fr[i][j], " ? ");
-              reponse = input()
+              print(Fr[i][j])
+              reponse = input(" ? ")
               if (En[i][j] != reponse):
                      print("la bonne réponse est : ", En[i][j])
               else:
