2a11f6fa551997c90d3ff95a1c332114ee05ac6f
theo <theomorneau@icloud.com>
2023-10-18 11:32:00 +0200

    interro 3 v2

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from random import *
+from tkinter import *
+"""
+fenetre = Tk()
+
+label = Label(fenetre, text="Texte par défaut", bg="yellow")
+label.pack()
+bouton = Button(fenetre, text="Fermer", command=fenetre.quit)
+bouton.pack()
+
+fenetre.mainloop()"""
+
 
 Fr = [["annonce","qui en a assez de","s'excuser de","responsable de","payer","agressivement","peu soucieux de","se renseigner sur","adjacent","ajuster"]
       ,["rappeler quelque chose a quelqu'un","ajustement","rire de","admirer","faché de/avec","admettre","avancé","poser sa candidature","candidat","chaine"]
@@ -23,12 +34,12 @@ En = [["annoucement","tired of","apologize for","responsible for","pay for","agg
       ,["base","accomplishment","beneficial","accumulate","accumulation","beverage","budget","acquerire","calculation","action"]]
 
 
-def ajouterMot():
+def ajouterMot(connu):
     mot = []
     while (len(mot) <= 10):
         i = randint(0, 9)
         j = randint(0, len(En[i]) - 1)
-        if (Fr[i][j], En[i][j]) not in mot:
+        if (Fr[i][j], En[i][j])  not in mot and (Fr[i][j], En[i][j]) not in connu:
             mot.append((Fr[i][j], En[i][j]))
     return mot
 
@@ -37,23 +48,26 @@ continuer = "oui"
 manche = 1
 aReviser = []
 score = 0
+connu = []
 
 while (continuer == "oui"):
     mot = []
-    mot = ajouterMot()
+    mot = ajouterMot(connu)
     for i in range(0, 10):
         print(mot[i][0], " ?")
         reponse = input()
         if (mot[i][1] != reponse):
             print("la bonne réponse est : ", mot[i][1])
             print("")
-            aReviser.append(mot[i])
+            if (mot[i] not in aReviser):
+                aReviser.append(mot[i])
         else:
             print("bien joué ! \n")
+            connu.append(mot[i])
             score += 1
-    print((score/manche)*100, "%")
+    print((score/manche)*10, "%")
     manche += 1
-    if score != 10:
+    if (score/manche)*10 != 100:
         print("vous devez reviser : \n")
         for k in range(0, len(aReviser)):
             print(aReviser[k][1], " qui veux dire ", aReviser[k][0])
