1d19e037c50091d6b79b98557fd9afc51fa51fb1
theo <theomorneau@icloud.com>
2023-11-07 21:49:58 +0100

    interro 4 v1 j'ai ajouter des simplification pour remplir les listes

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 from random import *
-from tkinter import *
-"""
+"""from tkinter import *
+
 fenetre = Tk()
 
-label = Label(fenetre, text="Texte par défaut", bg="yellow")
-label.pack()
-bouton = Button(fenetre, text="Fermer", command=fenetre.quit)
-bouton.pack()
+fenetre.title("anglais")
+fenetre.geometry("600x600")
+fenetre.minsize(200, 200)
+fenetre.config(background='white')
 
-fenetre.mainloop()"""
+label_title = Label(fenetre, text="test")
+label_title.pack()
 
+fenetre.mainloop()
+"""
 
-Fr = [["annonce","qui en a assez de","s'excuser de","responsable de","payer","agressivement","peu soucieux de","se renseigner sur","adjacent","ajuster"]
-      ,["rappeler quelque chose a quelqu'un","ajustement","rire de","admirer","faché de/avec","admettre","avancé","poser sa candidature","candidat","chaine"]
-      ,["adherence","capasité","adherent","abordable","demander","agenda","tenir à","agent","agression","adhérer"]
-      ,["désolé de","désolé de","allouer","attendre","catégorie","alternative","adresse","content de","annuelment","anxiété"]
-      ,["anxieux","certain de","appétit","inquiet au sujet de","rire de","application","appliquer","parler de","apprécier","qui connait bien"]
-      ,["appréhensif","content de","apprenti","aller avec","approche","s'occuper de","arranger","appartenir à","arrangement","contre,opposé à"]
-      ,["aspect","similaire à","assembler","écouter","assistance","attendre avec impatience","associer","parler à","assurance","dépendre de"]
-      ,["assurer","intéressé par","attitude","réussir","attirer","abondant","attirant","accepter","attrayant","acceptable"]
-      ,["audience","accès","accessibles","autoriser","automatique","accomodant","automatiquement","accompagner","automatisation","accomplir"]
-      ,["base","accomplissement","bénéfique","accumuler","accumulation","breuvage","budget","acquérir","calcul","action"]]
+Fr = [["A l'étranger","selon,suivant","comptabilité","précision","reconnaitre , accuser de recéption","faire de la pub","conseiller","diminuer , s'aténuer","logement","justifier , expliquer , représenter"]
+      ,["comptable","comptes","accomplir , atteindre","en fait","publicité ,annonce","publicité","conseil","toucher,influencer","avoir les moyens , se permettre","abordable , pas chère"]
+      ,["désolé","après-vente","ordre du jour","but / objectif , vise , avoir un but","climatisation","allées(magasin),couloir(avion)","réveil","permettre","modifier,changer","bien que"]
+      ,[]
+      ,[]
+      ,[]
+      ,[]
+      ,[]
+      ,[]
+      ,[]]
 
-En = [["annoucement","tired of","apologize for","responsible for","pay for","aggresively","careless about","ask about","adjacent","adjust"]
-      ,["remind somebody of","adjustement","laught at","admire","angry at","admit","advanced","apply for","candidate","chain"]
-      ,["adherence","capasity","adherent","affordable","ask for","agenda","care for","agent","aggression","adhere"]
-      ,["sorry about","sorry for","allocate","wait for","category","alternative","addresse","happy about","annualy","anxiety"]
-      ,["anxious","sure about","appetite","worried about","laught about","application","apply","talk about","appreciate","familiar with"]
-      ,["apprehensive","happy with","apprentice","belong with","approach","deal with","arrange","belong to","arrangement","opposed to"]
-      ,["aspect","similar to","assemble","listen to","assist","look forward to","associate","talk to","assurance","depend on"]
-      ,["assure","insit in","attitude","suceed in","attract","abundant","attraction","accept","attractive","acceptable"]
-      ,["audience","access","accessible","authorize","automatic","accommodating","automatically","accompany","automation","accomplish"]
-      ,["base","accomplishment","beneficial","accumulate","accumulation","beverage","budget","acquerire","calculation","action"]]
+En = [["abroad","according to","accounting","accuracy","acknowledge","advertise","advise","abate","accommodation","account for"]
+      ,["accountant","accounts","achieve","actually","ad,advertisement","advertising","advice","affect","afford","affordable"]
+      ,["afraid","after-sales","agenda","aim","air conditioning","aisle","alarm clock","allow","alter","although"]
+      ,[]
+      ,[]
+      ,[]
+      ,[]
+      ,[]
+      ,[]
+      ,[]]
 
+for i in range(0,9):
+        if (not(len(En[i]) == len(Fr[i]))):
+            print(i , len(En[i]), len(Fr[i]))
 
+s = d = 0
+for i in range(0,9):
+      s += len(En[i])
+      d += len(Fr[i])
+
+if (s != 100 or d != 100):
+      print(s,d)
+
+"""
 def ajouterMot(connu):
     mot = []
     if (len(connu)<90):
@@ -80,3 +95,4 @@ while (continuer == "oui"):
 print("vous devez reviser : \n")
 for k in range(0, len(aReviser)):
     print(aReviser[k][1], " qui veux dire ", aReviser[k][0])
+"""
