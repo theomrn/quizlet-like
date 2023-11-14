3fdd779ef198ae3e8c042d5c4ceadae54139dde9
theo <theomorneau@icloud.com>
2023-11-14 22:32:43 +0100

    interro 5 v1

--- main.py
+++ main.py
@@ -15,48 +15,29 @@ label_title.pack()
 fenetre.mainloop()
 """
 
-Fr = [["A l'étranger", "selon,suivant", "comptabilité", "précision", "reconnaitre , accuser de recéption",
-       "faire de la pub", "conseiller", "diminuer , s'aténuer", "logement", "justifier , expliquer , représenter"]
-    , ["comptable", "comptes", "accomplir , atteindre", "en fait", "publicité ,annonce", "publicité", "conseil",
-       "toucher,influencer", "avoir les moyens , se permettre", "abordable , pas chère"]
-    , ["désolé", "après-vente", "ordre du jour", "but / objectif , vise , avoir un but", "climatisation",
-       "allées(magasin),couloir(avion)", "réveil", "permettre", "modifier,changer", "bien que"]
-    , ["parmi,entre", "somme,quantité,montant", "mécontent,contrarié", "responsable devant", "répondeur",
-       "inquiet,désireux de", "s'excuser", "vêtements", "appel(juridique),attirer", "attirant"]
-    , ["appareil", "candidat", "candidature", "demande,postuler,solliciter", "nommmer quelqu'un", "rendez vous",
-       "évaluation (de sa performance au travail)", "approbation,assentiment", "se disputer", "dispute"]
-    , ["organiser", "arreter quelqu'un", "a partir de", "a condition de", "evaluer", "un atout,un bien",
-       "mission,devoir", "aider", "distruteur de billet", "assister a quelque cbose"]
-    , ["présence,participation", "vetements", "avocat(a la cour)", "attirer", "vente aux enchères", "disponibilité",
-       "disponible", "éviter", "récompenser,prix", "primé,qui gagne un prix"]
-    , ["conscient", "conscience", "lot", "avoir du retard", "ne pas manquer de", "avoir le droit a",
-       "chargé de,responsable de", "qui vaut", "se comporter", "comportement"]
-    , ["bagagiste,groom", "appartenir,aller,se ranger", "affaires,effets personnels", "sous,au-dessous de", "banc",
-       "plier", "avantages", "a coté de", "de plus,en plus", "boisson"]
-    , ["partial,parti pris", "offrir,faire une enchère de,appel d'offre,enchère", "facture,addition",
-       "plan,prototype,blues", "planche,embarquer", "conseil d'administration", "salle de reunion",
-       "attacher,verrouiller,écrous", "capot", "reserver"]]
+Fr = [["Comptabilité","s'ennyer,ennuyeux","A destination de","Frein,Freiner","Succursale","Marque (d'un produit)","tout neuf","pause,casser","Tomber en panne","Cambrioler,cambriolage"]
+    , ["percer","découverte,percée","porte-document,attaché-case","baisser","avancer","sortir,lancer,faire paraître","élever (un enfant)","ADSL","diffiser,diffusion","courtier"]
+    , ["balai","feuilleter,parcourir","en gros (vente)","boucher","placard","appeler,un appel","rappeler au telephone","annuler","annuler2","Annulation"]
+    , ["ne pas s'empecher","détester,ne pas supporter","continuer à","réaliser,faire","attraper","fournir la nourriture,s'adresser a","traiteur","restauration,buffet","chauffage central","directeur général"]
+    , ["citoyen","citoyenneté,nationalité","présider","president","faire payer,frais","accuser,inculper","tableau,courbe graphique","pas cher","verifier,controler","compte courant"]
+    , ["se présenter à l'enregistrement","regler sa note et rendre la chambre d'hotel","nourrice","hâcher","revendication,reclamation,declaration de sinistre","degager,defricher","employé","intelligent","fermer définitivement","monter,grimper"]
+    , ["s'écrouler,s'éffondrer","baisser","sortir","sugir,apparaitre","avoir/trouver/penser à","pub à la radio/tv","engagement","trajet,voyager(tous les jours)","banlieusard(qui fait la navette)","concurrence"]
+    , ["concurrent","se plaindre","plaite","achèvement,aboutissement","gratuit","conforme à","se soumettre,respecter,etre conforme à","inquiet","mener","se conformer à"]
+    , ["feliciter quelqu'un","envoi","travaux","récipient","entrepeneur","comptoir","quelques","coursier","fomation","couverture"]
+    , ["artisant","se planter,s'effondrer","equipe,equipage","culture,recolte","foule","bondé,plein de gens","croisière","monnaie,devise","actuel,actuellement,en ce moment","client"]]
 
-En = [
-    ["abroad", "according to", "accounting", "accuracy", "acknowledge", "advertise", "advise", "abate", "accommodation",
-     "account for"]
-    , ["accountant", "accounts", "achieve", "actually", "ad,advertisement", "advertising", "advice", "affect", "afford",
-       "affordable"]
-    ,
-    ["afraid", "after-sales", "agenda", "aim", "air conditioning", "aisle", "alarm clock", "allow", "alter", "although"]
-    , ["among", "amount", "annoyed", "answerable", "answering machine", "anxious", "apologize for", "apparel", "appeal",
-       "appealing"]
-    , ["appliance", "applicant", "application", "apply for", "appoint", "appointment", "appraisal", "approval", "argue",
-       "argument"]
-    , ["arrange", "arrest", "as of", "as/so long as", "assess", "asset", "assignment", "assist", "atm", "attend"]
-    , ["attendance", "attire", "attorney", "attract", "auction", "availability", "available", "avoid", "award",
-       "award-winning"]
-    , ["aware", "awareness", "batch", "be behind with something", "be bound", "be entitled to", "be in charge of",
-       "be worth", "behave", "behaviour"]
-    , ["bellhop,bellman", "belong", "belongings", "below", "bench", "bend", "benefits", "beside", "besides", "beverage"]
-    , ["biased", "bid", "bill", "blueprint", "board", "board of directors", "board room", "bolt", "bonnet", "book"]]
+En = [["book-keeping","bore,boring","bound","brake","branch","brand name","brand new","break","break down","break in"]
+    ,["break into","breakthrough","briefcase","bring down","bring forward","bring out","bring up","broadband","broadcast","broker"]
+    ,["broom","browse","bulk","butcher","cabinet","call","call back","call off","cancel","cancelation"]
+    ,["can't help","can't stand","carry on","carry out","catch","cater","caterer","catering","central heating","ceo"]
+    ,["citizen","citizenship","chair","chairman","charge","charge somebody with something","chart","cheap","check","checking account"]
+    ,["check in","check out","childminder","chop","claim","clear","clerk","clever","close down","clim"]
+    ,["collapse","come down","come out","come up","compe up with","commercial","commitment","commute","commuter","competition"]
+    ,["competitor","complain","complait","completion","complimentary","compliant with","comply with","concerned","conduct","conform"]
+    ,["congratulate","consignment","construction","container","contractor","counter","couple","courier","course","coverage"]
+    ,["craftman,craftmen","crash","crew","crop","crowd","crowded","cruise","currency","current","customer"]]
 
-"""
+
 for i in range(0,10):
         if (not(len(En[i]) == len(Fr[i]))):
             print(i , len(En[i]), len(Fr[i]))
@@ -68,9 +49,9 @@ for i in range(0,10):
 
 if (s != 100 or d != 100):
       print(s,d)
+
+
 """
-
-
 def ajouterMot(connu):
     mot = []
     if (len(connu) < 90):
@@ -94,11 +75,11 @@ aReviser = []
 score = 0
 connu = []
 
-"""
+
 for i in range(0,10):
     aReviser = aReviser + En[i]
 print(aReviser)
-"""
+
 while (continuer == "oui"):
     mot = []
     mot = ajouterMot(connu)
@@ -122,3 +103,4 @@ while (continuer == "oui"):
 print("vous devez reviser : \n")
 for k in range(0, len(aReviser)):
     print(aReviser[k][1], " qui veux dire ", aReviser[k][0])
+"""
